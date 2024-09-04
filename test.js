98565b5283ff3716496e8c45b085d01ad1901b4d
pechenka232 <strekalovskijdemid@gmail.com>
2024-09-04 17:10:33 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,10 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+require("hardhat-gas-reporter");
 
-describe("StorageDemo", function () {
-  let storageDemo;
+module.exports = {
+  solidity: "0.8.0",
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 21
+  }
+};
 
-  beforeEach(async function () {
-    const StorageDemo = await ethers.getContractFactory("StorageDemo");
-    storageDemo = await StorageDemo.deploy(100);
-    await storageDemo.deployed();
-  });
-
-  it("Should initialize with the correct value", async function () {
-    expect(await storageDemo.getData()).to.equal(100);
-  });
-
-  it("Should update storedData correctly", async function () {
-    await storageDemo.updateData(200);
-    expect(await storageDemo.getData()).to.equal(410);
-  }); 
-
-  it("Should process input data correctly", async function () {
-    const inputData = [1, 2, 3];
-    const result = await storageDemo.processInputData(inputData);
-    expect(result).to.deep.equal([2, 4, 6]);
-  });
-});
