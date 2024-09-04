2d86f4a31f4f101cb47c7ec4258823319143ec1a
pechenka232 <strekalovskijdemid@gmail.com>
2024-09-04 17:08:33 +0800

    Update main.sol

--- main.sol
+++ main.sol
@@ -1,36 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+//обычный контракт
+contract StorageExample {
+    uint256 public storedValue;
 
-contract StorageDemo {
-    uint256 public storedData;  
-
-    constructor(uint256 _initialData) {
-        storedData = _initialData;
+    constructor(uint256 _initialValue) {
+        storedValue = _initialValue;
     }
 
-    function updateData(uint256 _newData) public {
-        storedData = _newData;  
-
-        uint256 tempData = storedData * 2;  
-        uint256 result = calculateData(tempData);  
-
-        storedData = result;
+    function updateStoredValue(uint256 _newValue) public {
+        uint256 tempValue = storedValue * 2;  // Использование storage и stack
+        storedValue = calculateValue(tempValue, _newValue);  // Использование memory и calldata
     }
 
-    function calculateData(uint256 _multiplier) public pure returns (uint256) {
-        uint256 data = _multiplier + 10;  
-        return data;
+    function calculateValue(uint256 _tempValue, uint256 _inputValue) public pure returns (uint256) {
+        uint256 result = _tempValue + _inputValue;  // Использование stack
+        return result;
     }
 
-    function processInputData(uint256[] calldata _inputData) external pure returns (uint256[] memory) {
-        uint256[] memory outputData = new uint256[](_inputData.length);                 
-        for (uint256 i = 0; i < _inputData.length; i++) {
-            outputData[i] = _inputData[i] * 2;
+    function processArray(uint256[] calldata _inputArray) external pure returns (uint256[] memory) {
+        uint256[] memory outputArray = new uint256[](_inputArray.length);  // Использование memory и calldata
+        for (uint256 i = 0; i < _inputArray.length; i++) {
+            outputArray[i] = _inputArray[i] * 2;
         }
-        return outputData;
+        return outputArray;
     }
 
-    function getData() public view returns (uint256) {
-        return storedData;
+    function getStoredValue() public view returns (uint256) {
+        return storedValue;  // Использование storage
+    }
+}
+//Оптимизированный контракт  
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract StorageOptimized {
+    uint256 public storedValue;
+
+    constructor(uint256 _initialValue) {
+        storedValue = _initialValue;
+    }
+
+    function updateStoredValue(uint256 _newValue) public {
+        storedValue = (storedValue * 2) + _newValue;  // Сокращение операций и переменных
+    }
+
+    function processArray(uint256[] calldata _inputArray) external pure returns (uint256[] memory) {
+        uint256[] memory outputArray = new uint256[](_inputArray.length);
+        for (uint256 i = 0; i < _inputArray.length; i++) {
+            outputArray[i] = _inputArray[i] * 2;  // Оптимизация использования calldata и memory
+        }
+        return outputArray;
+    }
+
+    function getStoredValue() public view returns (uint256) {
+        return storedValue;
     }
 }
